c6e0dc06987c1a1225339cd27996bc057597f2ce
Ubuntu <ubuntu@ip-172-31-32-141.ec2.internal>
2021-01-31 06:41:07 +0000

    up

--- WEB.sh
+++ WEB.sh
@@ -6,21 +6,21 @@
 # These will be substituted in JMX test script
 # See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
 
-export TARGET_TOKEN="c0bf1f21b271b8ad53dd846af507fc918ecc4286da"
-export TARGET_THREADS="20"
-export TARGET_LOOPS="1"
+export TARGET_TOKEN="25c0dd90fc4ac6c135df0d02ab5f24738ecc4286da"
+export TARGET_THREADS="200"
+export TARGET_LOOPS="5"
 export TARGET_TIME="60"
 
 T_DIR=tests/compra
 
 # Reporting dir: start fresh
-R_DIR=${T_DIR}/trreport-$(date +"%F-%H-%M")
+R_DIR=${T_DIR}/report-$(date +"%F-%H-%M")
 rm -rf ${R_DIR} > /dev/null 2>&1
 mkdir -p ${R_DIR}
 
 /bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
 
-./run.sh -Dlog_level.jmeter=DEBUG \
+./run2.sh -Dlog_level.jmeter=DEBUG \
 	-JTARGET_THREADS=${TARGET_THREADS} -JTARGET_TOKEN=${TARGET_TOKEN} \
 	-JTARGET_TIME=${TARGET_TIME} -JTARGET_LOOPS=${TARGET_LOOPS} \
 	-n -t ${T_DIR}/PSWRSV-2021.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
