2acd290f2c15fa702796cb1204212fa331e0a879
Jefferson Riobueno <riobueno.jr@gmail.com>
2024-02-28 01:11:43 -0500

    first commit

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+JMETER_VERSION=${JMETER_VERSION:-"5.6.3"}
+IMAGE_TIMEZONE=${IMAGE_TIMEZONE:-"America/Lima"}
+
+# Example build line
+docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ=${IMAGE_TIMEZONE} -t "riobueno/jmeter:${JMETER_VERSION}" .
